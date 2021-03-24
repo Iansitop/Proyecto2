0f9ca383dba2654bc351ba85005c672c8e1e42ce
crist <crist@DESKTOP-2G20CFT>
2021-03-23 23:27:39 -0700

    entidad clientes

---
+++ Proyecto2BDA/src/entidades/Clientes.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author crist
+ */
+@Entity
+@Table (name = "clientes")
+public class Clientes implements Serializable {
+
+    public Clientes() {
+    }
+    
+
+    public Clientes(int id, String rfc, String nombre, String direccion, String telefono1, String telefono2) {
+        this.id = id;
+        this.rfc = rfc;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono1 = telefono1;
+        this.telefono2 = telefono2;
+    }
+
+    public Clientes(String rfc, String nombre, String direccion, String telefono1, String telefono2) {
+        this.rfc = rfc;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono1 = telefono1;
+        this.telefono2 = telefono2;
+    }
+
+    
+    
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Column (name = "idcliente")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column (name = "rfc")
+    private String rfc;
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+    
+    @Column (name = "nombre")
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    @Column (name = "direccion")
+    private String direccion;
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    @Column (name = "telefono1")
+    private String telefono1;
+
+    public String getTelefono1() {
+        return telefono1;
+    }
+
+    public void setTelefono1(String telefono1) {
+        this.telefono1 = telefono1;
+    }
+    
+    @Column (name = "telefono2")
+    private String telefono2;
+
+    public String getTelefono2() {
+        return telefono2;
+    }
+
+    public void setTelefono2(String telefono2) {
+        this.telefono2 = telefono2;
+    }
+    
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (int) id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Clientes)) {
+            return false;
+        }
+        Clientes other = (Clientes) object;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Clientes{" + "id=" + id + ", rfc=" + rfc + ", nombre=" + nombre + ", direccion=" + direccion + ", telefono1=" + telefono1 + ", telefono2=" + telefono2 + '}';
+    }
+
+    
+}
